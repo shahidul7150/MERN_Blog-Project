58f8e76c2188354ad8d9099a02e830d854cd7a88
shahidul7150 <shahidulislam7150@gmail.com>
2022-09-13 22:37:15 +0600

    created comment component UI

--- frontend-site/src/components/home/Comment.jsx
+++ frontend-site/src/components/home/Comment.jsx
@@ -2,36 +2,188 @@ import React from 'react'
 import { BsTrash } from "react-icons/bs"
 const Comment = () => {
     return (
-        <div className="comments">
-            <div className="main-reply-comment">
-                <div className="image-comment-time-name">
-                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
-                    <div className="name-time-comment">
-                        <div className="name-time">
-                            <h4>Shahidul Islam</h4>
-                            <span>12 days ago</span>
-                        </div>
-                        <p>The difficulties of adversarial training are well-documented</p>
-                        <div className="reply-btn">
-                            <button>Reply</button>
-                        </div>
-                        <div className="reply-box">
-                            <div className="image-input">
-                                <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
-                                <input type="text" placeholder='add public reply' required />
+        <>
+            <div className="comments">
+                <div className="main-reply-comment">
+                    <div className="image-comment-time-name">
+                        <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                        <div className="name-time-comment">
+                            <div className="name-time">
+                                <h4>Shahidul Islam</h4>
+                                <span>12 days ago</span>
                             </div>
-                            <div className="reply-submit">
-                                <button className='cancle'>Cancle</button>
-                                <button className='submit'>Submit</button>
+                            <p>The difficulties of adversarial training are well-documented</p>
+                            <div className="reply-btn">
+                                <button>Reply</button>
+                            </div>
+                            <div className="reply-box">
+                                <div className="image-input">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <input type="text" placeholder='add public reply' required />
+                                </div>
+                                <div className="reply-submit">
+                                    <button className='cancle'>Cancle</button>
+                                    <button className='submit'>Submit</button>
+                                </div>
+                            </div>
+                            <div className="reply-comment">
+                                <div className="reply-comment-image-name-time">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <div className="name-time-comment">
+                                        <div className="name-time">
+                                            <h4>Shahidul Islam</h4>
+                                            <span>12 days ago</span>
+                                        </div>
+                                        <p>The difficulties of adversarial training are well-documented</p>
+                                        <div className="reply-btn">
+                                            <button>Reply</button>
+                                        </div>
+                                        <div className="reply-box">
+                                            <div className="image-input">
+                                                <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                                <input type="text" placeholder='add public reply' required />
+                                            </div>
+                                            <div className="reply-submit">
+                                                <button className='cancle'>Cancle</button>
+                                                <button className='submit'>Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="action">
+                                    <BsTrash />
+                                </div>
+                            </div>
+                            <div className="reply-comment">
+                                <div className="reply-comment-image-name-time">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <div className="name-time-comment">
+                                        <div className="name-time">
+                                            <h4>Shahidul Islam</h4>
+                                            <span>12 days ago</span>
+                                        </div>
+                                        <p>The difficulties of adversarial training are well-documented</p>
+                                        <div className="reply-btn">
+                                            <button>Reply</button>
+                                        </div>
+                                        <div className="reply-box">
+                                            <div className="image-input">
+                                                <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                                <input type="text" placeholder='add public reply' required />
+                                            </div>
+                                            <div className="reply-submit">
+                                                <button className='cancle'>Cancle</button>
+                                                <button className='submit'>Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="action">
+                                    <BsTrash />
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="action">
+                        <BsTrash />
+                    </div>
                 </div>
-                <div className="action">
-                    <BsTrash />
+                <div className="main-reply-comment">
+                    <div className="image-comment-time-name">
+                        <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                        <div className="name-time-comment">
+                            <div className="name-time">
+                                <h4>Shahidul Islam</h4>
+                                <span>12 days ago</span>
+                            </div>
+                            <p>The difficulties of adversarial training are well-documented</p>
+                            <div className="reply-btn">
+                                <button>Reply</button>
+                            </div>
+                            <div className="reply-box">
+                                <div className="image-input">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <input type="text" placeholder='add public reply' required />
+                                </div>
+                                <div className="reply-submit">
+                                    <button className='cancle'>Cancle</button>
+                                    <button className='submit'>Submit</button>
+                                </div>
+                            </div>
+                            <div className="reply-comment">
+                                <div className="reply-comment-image-name-time">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <div className="name-time-comment">
+                                        <div className="name-time">
+                                            <h4>Shahidul Islam</h4>
+                                            <span>12 days ago</span>
+                                        </div>
+                                        <p>The difficulties of adversarial training are well-documented</p>
+                                        <div className="reply-btn">
+                                            <button>Reply</button>
+                                        </div>
+                                        <div className="reply-box">
+                                            <div className="image-input">
+                                                <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                                <input type="text" placeholder='add public reply' required />
+                                            </div>
+                                            <div className="reply-submit">
+                                                <button className='cancle'>Cancle</button>
+                                                <button className='submit'>Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="action">
+                                    <BsTrash />
+                                </div>
+                            </div>
+                            <div className="reply-comment">
+                                <div className="reply-comment-image-name-time">
+                                    <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                    <div className="name-time-comment">
+                                        <div className="name-time">
+                                            <h4>Shahidul Islam</h4>
+                                            <span>12 days ago</span>
+                                        </div>
+                                        <p>The difficulties of adversarial training are well-documented</p>
+                                        <div className="reply-btn">
+                                            <button>Reply</button>
+                                        </div>
+                                        <div className="reply-box">
+                                            <div className="image-input">
+                                                <img src="http://localhost:3000/articleImage/office.jpg" alt="" />
+                                                <input type="text" placeholder='add public reply' required />
+                                            </div>
+                                            <div className="reply-submit">
+                                                <button className='cancle'>Cancle</button>
+                                                <button className='submit'>Submit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="action">
+                                    <BsTrash />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="action">
+                        <BsTrash />
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className="comment-submit">
+                <h2>Give Your Comment</h2>
+                <div className="form-group">
+                    <textarea className='form-control' placeholder='Write a comment' name="" id="" cols="20" rows="10"></textarea>
+
+                </div>
+                <div className="form-group">
+                    <button className="btn">Submit</button>
+                </div>
+            </div>
+        </>
     )
 }
 
